aad32bc5dbea2aa86928be2f897cadf2e432e672
gaopengxiang417 <gaopengxiang417@163.com>
2014-05-17 23:41:41 +0800

    添加了基本的运算符和操作数

--- com/gao/basic/basic.py
+++ com/gao/basic/basic.py
@@ -17,7 +17,6 @@ print "the last lineyes \
 #如果一个字符换前面加r或者R标示是自然字符串，不需要进行转义
 print r"newlines are created by ss\nss"
 
-
 print "中国"
 
 print u"unicode character 高鹏翔"
@@ -33,5 +32,29 @@ print i
 
 s = "this is a multi linestring" \
     "new line"
-print  s
+print s
 
+print("check")
+print("ehck")
+
+#运算符和操作数
+print "********start to alg**********"
+print 3 + 5  #加法
+print 3 - 5  #减法
+print 3 * 2  #乘法
+print 3 * "l" #乘法
+print 3 ** 3    #阶乘
+print 4 / 3     #除法
+print 4.0 / 3   #除法保留小数
+print 4.0 // 3  #除法只输出整数
+print 4 % 2     #莫
+print 3 << 2
+print 3 >> 2
+print 3 & 1
+print 3 | 2
+print 3 < 4
+print not True  #反向
+print 3 != 4
+print 3 == 4
+print True and False
+print True or False
