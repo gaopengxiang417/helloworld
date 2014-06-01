4879b70596dc05ee9f083e0f4f0c0b66358d9fa3
gaopengxiang417 <gaopengxiang417@163.com>
2014-06-01 14:52:52 +0800

    hard way learn

---
+++ com/gao/hardway/exs1.py
@@ -0,0 +1,23 @@
+__author__ = 'wangchen'
+#coding=utf-8
+
+print "Hello world"
+print "hello again"
+print "i like typing this"
+print "this is fun"
+print "yes,printing"
+print "i would rather than you 'not'"
+print 'i said "not " to touch that'
+
+print "add a line"
+# print "this line is comment"
+
+#a comment , so you can read your program later
+#anything after the # is ignored by python
+
+print "I could have code like this." # and comment after this is ignored
+
+# you can use comment to disable  or comment a piece of code
+# print "this will not be print"
+
+print "This will run"
