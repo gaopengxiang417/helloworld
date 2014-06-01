a65dacffc56a4a02ceb308704a9ae9f3015ab5a4
gaopengxiang417 <gaopengxiang417@163.com>
2014-06-01 15:01:40 +0800

    添加了对于运算符的功能学习

--- com/gao/hardway/exs1.py
+++ com/gao/hardway/exs1.py
@@ -21,3 +21,23 @@ print "I could have code like this." # and comment after this is ignored
 # print "this will not be print"
 
 print "This will run"
+
+# i will count the eggs
+
+print "Hens:", 25 + 30 / 6
+print "Roosters:", 100 - 25 * 3 % 4
+
+# i will count the chickens
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "is that true 3 + 2 < 5 - 7", 3 + 2 < 5 - 7
+print "what's that 3 + 2:", 3 + 2
+print "what's that 5 - 7:", 5 - 7
+print "that's why it's false"
+
+print "how about some more"
+print "is that greator: ", 5 > 12
+print "is greator or equal:", 5 >= -4
+print "is less or equal:", 5 <= -2
+
+print 30.0 / 13
