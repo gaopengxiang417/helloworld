f28ce43ebb2be28daed7d96ec3f643b637bf18a0
gaopengxiang417 <gaopengxiang417@163.com>
2014-07-13 22:57:48 +0800

    boolean表达式判断

---
+++ com/gao/hardway/booleanTest.py
@@ -0,0 +1,21 @@
+__author__ = 'wangchen'
+#coding=utf-8
+
+
+print True and False
+print False and True
+print 1 == 1 and 2 ==1
+print "test" == "test"
+print 1 == 1 or 2 != 1
+print True and 1 == 1
+print False and 0 != 1
+print True or 1 == 1
+print "test" == "testing"
+print "test" == 1
+print not (True and False)
+print (1 == 1 and 0 != 1)
+print not (10 == 1 or 1000 == 1000)
+print not (1 != 10 or 3 == 4)
+print not ("testing" == "testing" and "zed" == "hello")
+print 1 == 1 and not ("testing" == 1 or 1 ==0)
+print "chunk" == "any" and not (3 ==4 or 3 == 3)
