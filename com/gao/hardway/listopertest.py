265244843b3c0af052d9c5d59723aeedc87d2429
gaopengxiang417 <gaopengxiang417@163.com>
2014-07-21 22:33:18 +0800

    list的操作功能

---
+++ com/gao/hardway/listopertest.py
@@ -0,0 +1,13 @@
+__author__ = 'wangchen'
+#coding=utf-8
+
+#测试对于列表的操作功能
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+
+print "the lst animal is at 0,and is a ", animals[0]
+print "the 3rd animal is at 2,and is a ", animals[2]
+print "the lst animal is at 0,and is a ", animals[0]
+print "the 4th animal is at 3,and is a ", animals[3]
+print "the 5th animal is at 4,and is a ", animals[4]
+print "the 4th animal is at 3,and is a ", animals[3]
+print "the 6th animal is at 5,and is a ", animals[5]
