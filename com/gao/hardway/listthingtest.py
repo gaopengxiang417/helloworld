fe6e87dc2c1319267a6159ae638f172b28612ce0
gaopengxiang417 <gaopengxiang417@163.com>
2014-07-26 15:19:44 +0800

    对于list的操作和join函数使用

---
+++ com/gao/hardway/listthingtest.py
@@ -0,0 +1,30 @@
+__author__ = 'wangchen'
+#coding=utf-8
+
+ten_thing = "apples oranges crows telephone light sugar"
+
+print "wait there's not 10 things in that list,let's fix that"
+
+stuff = ten_thing.split(" ")
+more_stuff = ["day", "night", "friebee", "corn", "banana", "girl", "boy"]
+
+while len(stuff) != 10:
+    next = more_stuff.pop()
+    print "adding ", next
+    stuff.append(next)
+    print "there is %d items there" % len(stuff)
+
+
+print "there we go:" , stuff
+
+print "let's do something with stuff"
+
+print stuff[1]
+
+print stuff[-1]
+
+print stuff.pop()
+
+print " ".join(stuff)
+
+print "#".join(stuff[3:5])
