d3369d1c1f0b1229d0a6446aa683061a5da88edb
gaopengxiang417 <gaopengxiang417@163.com>
2014-08-02 11:47:22 +0800

    class的学习

--- com/gao/hardway/classtest.py
+++ com/gao/hardway/classtest.py
@@ -28,3 +28,33 @@ print b.add_me_up(5)
 print TheThing.__doc__
 print a.__init__().__doc__
 
+
+#animal is a object
+class Animal(object):
+    pass
+
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+class Person(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.pet = None
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        super(Employee, self).__init__(name)
+        self.salary = salary
+
+class Fish(object):
+    pass
+
+
+
